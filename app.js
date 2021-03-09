873e31695cb3f06af2245e5eb5c94ac67a904710
ALi <s.arme@ya.ru>
2021-03-09 18:17:32 +0300

    👷 Add liveness and readiness to the app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const health = require('@cloudnative/health-connect');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/live', health.LivenessEndpoint(healthcheck))
+app.use('/ready', health.ReadinessEndpoint(healthcheck))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
