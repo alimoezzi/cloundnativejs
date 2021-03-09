80ea2187aba36eb9fc8fc3063e8985085ae5cb37
ALi <s.arme@ya.ru>
2021-03-09 18:32:07 +0300

    🐛 Fix healthcheck was not defined

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const health = require('@cloudnative/health-connect');
+let healthcheck = new health.HealthChecker();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
