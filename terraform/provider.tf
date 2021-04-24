55c576daa210cb3798e32d07c09df66f088ac635
ALi <s.arme@ya.ru>
2021-04-24 22:50:49 +0300

    🚀 Add terraform providers

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  version = "~> 3.16.0"
+  region  = var.region
+  project = var.project
+}
+
+provider "random" {
+  version = "~> 2.2.1"
+}
+
+provider "null" {
+  version = "~> 2.1.2"
+}
+
+provider "kubernetes" {
+  version = "~> v1.11.3"
+}
